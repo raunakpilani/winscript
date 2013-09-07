110968afa19ffac1c62f9510a030079514240207
Raunak Pilani <raunak@browserstack.com>
2013-09-07 19:28:33 +0530

    add disable taskband

--- disable_access.bat
+++ disable_access.bat
@@ -469,4 +469,4 @@ REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v No
 :: Prevent Start Menu from opening
 REG DELETE "HKLM\SOFTWARE\Classes\CLSID\{a2a9545d-a0c2-42b4-9708-a0b2badd77c8}" /f
 :: Remove Taskband
-REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /va /f
+REG DELETE "HKLM\SOFTWARE\Classes\*\shellex\ContextMenuHandlers\{90AA3A4E-1CBA-4233-B8BB-535773D48449}" /f
